906a26950684466f2e3864bf24c7f934a6770503
SaharMohajeri <saharmohajeri25@gmail.com>
2025-07-18 04:41:29 +0330

    map1

--- src/engine/map1.py
+++ src/engine/map1.py
@@ -15,12 +15,14 @@ fighters = pygame.sprite.Group()        # Contains all fighter objects
 
 
 static_platform1 = Platform(config.SCENE_WIDTH*2/8 + 73, config.SCENE_HEIGHT*3/5 - 15, 
-                           config.SCENE_WIDTH*3/8,config.SCENE_HEIGHT*1/100, 
+                           config.SCENE_WIDTH*3/8,config.SCENE_HEIGHT*1/200, 
                            color=(139,69,19))
-static_platform2 = Platform(config.SCENE_WIDTH*2/8, config.SCENE_HEIGHT*1/5 - 15, 
-                           100,config.SCENE_HEIGHT*1/100, 
+static_platform2 = Platform(config.SCENE_WIDTH*2/8 + 88, config.SCENE_HEIGHT*2/5 + 13, 
+                           130,config.SCENE_HEIGHT*1/200, 
                            color=(139,69,19))
-
+static_platform3 = Platform(680, config.SCENE_HEIGHT*2/5 + 13, 
+                           130,config.SCENE_HEIGHT*1/200, 
+                           color=(139,69,19))
 
 # Two fighter objects using custom control keys for movement, jumping, and shooting.
 fighter1 = Fighter(450,  
@@ -43,8 +45,8 @@ enemy = NPC(static_platform1.rect.x + 30,
 enemy.add_animation("idle", "src/assets/images/enemies/Goblin/Run.png",150,150,crop_x= 60,crop_y= 65, crop_width=30, crop_height=35)
 enemy.add_animation("death", "src/assets/images/enemies/Goblin/Death.png",150,150,crop_x= 60,crop_y= 65, crop_width=30, crop_height=35)
 # Add each object to the appropriate sprite groups for updating and drawing
-all_sprites.add(static_platform1, static_platform2, fighter1, fighter2, enemy)
-platforms.add(static_platform1, static_platform2)
+all_sprites.add(static_platform1, static_platform2,static_platform3, fighter1, fighter2, enemy)
+platforms.add(static_platform1, static_platform2, static_platform3)
 enemies.add(enemy)
 fighters.add(fighter1, fighter2)
 
