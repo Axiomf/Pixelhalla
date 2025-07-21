be8de3af6521dc049b78f4cc13071fd6b0aa3dfd
Axiomf <nafariafshar@gmail.com>
2025-07-21 09:27:03 +0330

    improved and hiden the platforms of map goblin

--- src/engine/map1.py
+++ src/engine/map1.py
@@ -20,15 +20,16 @@ fighters = pygame.sprite.Group()        # Contains all fighter objects
 melee = pygame.sprite.Group()
 
 
-static_platform1 = Platform(config.SCENE_WIDTH*2/8 + 73, config.SCENE_HEIGHT*3/5 - 15, 
+static_platform1 = Platform(config.SCENE_WIDTH*2/8 + 73, config.SCENE_HEIGHT*3/5 - 11, 
                            config.SCENE_WIDTH*3/8,config.SCENE_HEIGHT*1/200, 
-                           color=(139,69,19))
+                           color=None)#this is the main platform
+
 static_platform2 = Platform(config.SCENE_WIDTH*2/8 + 88, config.SCENE_HEIGHT*2/5 + 13, 
                            130,config.SCENE_HEIGHT*1/200, 
-                           color=(139,69,19))
-static_platform3 = Platform(680, config.SCENE_HEIGHT*2/5 + 13, 
+                           color=None)
+static_platform3 = Platform(680, config.SCENE_HEIGHT*2/5 + 14, 
                            130,config.SCENE_HEIGHT*1/200, 
-                           color=(139,69,19))
+                           color=None)
 path_list = {"idle" : "src/assets/images/inused_sheets/Goblin/Idle.png",
              "walk" : "src/assets/images/inused_sheets/Goblin/Run.png",
              "death" : "src/assets/images/inused_sheets/Goblin/Death.png",
@@ -39,9 +40,7 @@ path_list = {"idle" : "src/assets/images/inused_sheets/Goblin/Idle.png",
 fighter1 = Fighter(450, config.SCENE_HEIGHT*3/5 - 70, 32, 32, platforms=platforms,
                    controls={"left": pygame.K_a, "right": pygame.K_d, "jump": pygame.K_w, "shoot": pygame.K_SPACE},
                    animations=load_animations_Arcane_Archer(scale=1))
-fighter2 = Fighter(static_platform1.rect.x + 30, static_platform1.rect.y, 32, 32,platforms=platforms,
-                   controls={"left": pygame.K_LEFT, "right": pygame.K_RIGHT, "jump": pygame.K_UP},
-                   animations=load_animations_Suicide_Bomber(40, 32))
+
 
 # An enemy that patrols horizontally and bounces at screen edges
 enemy = Melee(static_platform1.rect.x + 60,  
@@ -53,11 +52,11 @@ enemy = Melee(static_platform1.rect.x + 60,
             animations=load_animations_Goblin(path_list, 150,150,crop_x= 60,crop_y= 65, crop_width=30, crop_height=35))
 
 # Add each object to the appropriate sprite groups for updating and drawing
-all_sprites.add(static_platform1, static_platform2,static_platform3, fighter1, fighter2, enemy)
+all_sprites.add(static_platform1, static_platform2,static_platform3, fighter1, enemy)
 platforms.add(static_platform1, static_platform2, static_platform3)
 enemies.add(enemy)
 melee.add(enemy)
-fighters.add(fighter1, fighter2)
+fighters.add(fighter1)
 
 def draw_background():
     scene.fill((0, 0, 0))
